526f33a7bb147795427262e1591fdb2d5aba53ee
JQL <john@jql.co.uk>
2021-05-27 11:40:08 +0200

    Added ActionColumn4 to use Bootstrap Icons

--- views/country/_search.php
+++ views/country/_search.php
@@ -1,34 +1,36 @@
 <?php
 
 use yii\helpers\Html;
-use yii\widgets\ActiveForm;
+use yii\bootstrap4\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\CountrySearch */
-/* @var $form yii\widgets\ActiveForm */
+/* @var $form yii\bootstrap4\ActiveForm */
 ?>
 
 <div class="country-search">
 
-    <?php $form = ActiveForm::begin([
-        'action' => ['index'],
-        'method' => 'get',
-        'options' => [
-            'data-pjax' => 1
-        ],
-    ]); ?>
+  <?php
+  $form = ActiveForm::begin([
+      'action' => ['index'],
+      'method' => 'get',
+      'options' => [
+        'data-pjax' => 1
+      ],
+  ]);
+  ?>
 
-    <?= $form->field($model, 'code') ?>
+  <?= $form->field($model, 'code') ?>
 
-    <?= $form->field($model, 'name') ?>
+  <?= $form->field($model, 'name') ?>
 
-    <?= $form->field($model, 'population') ?>
+  <?= $form->field($model, 'population') ?>
 
-    <div class="form-group">
-        <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
-        <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-outline-secondary']) ?>
-    </div>
+  <div class="form-group">
+    <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
+    <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-outline-secondary']) ?>
+  </div>
 
-    <?php ActiveForm::end(); ?>
+  <?php ActiveForm::end(); ?>
 
 </div>
--- views/country/list.php
+++ views/country/list.php
@@ -16,7 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
   <h1><?= Html::encode($this->title) ?></h1>
 
   <p>
-<?= Html::a(Yii::t('app', 'Create Country'), ['create'], ['class' => 'btn btn-success']) ?>
+    <?= Html::a(Yii::t('app', 'Create Country'), ['create'], ['class' => 'btn btn-success']) ?>
   </p>
 
   <?php Pjax::begin(); ?>
@@ -29,6 +29,6 @@ $this->params['breadcrumbs'][] = $this->title;
   ]);
   ?>
 
-<?php Pjax::end(); ?>
+  <?php Pjax::end(); ?>
 
 </div>
